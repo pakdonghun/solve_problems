674b6325bec23b86790b1f4f8f42c690c2171df3
pakdonghun <66273924+pakdonghun@users.noreply.github.com>
2021-02-26 06:11:37 +0900

    Update 두 개 뽑아서 더하기.py

--- 두 개 뽑아서 더하기.py
+++ 두 개 뽑아서 더하기.py
@@ -1,7 +1,8 @@
 def solution(numbers):
     answer = []
     for i in range(len(numbers)):
-        for j in range(i+1,len(numbers)):
-            answer.append(numbers[i] + numbers[j])
+        for j in range(len(numbers)):
+            if i != j:
+                answer.append(numbers[i] + numbers[j])
     answer = sorted(list(set(answer)))
     return answer
